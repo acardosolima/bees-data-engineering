ce4cc050bd5ffbde1e813c4141e37e139c69a776
Adriano <adrianocardoso1991@gmail.com>
2024-08-06 13:37:31 -0300

    Chore: setup logging config

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
+import logging.config
+
+# Setup logging configuration
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger(__name__)
+
 def main():
+    logger.info("Logging is working")
     return "main"
 
 if __name__ == "__main__":
