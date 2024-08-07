9286b7fd81dc6e491b1ee60a79e809ee8a106018
Adriano <adrianocardoso1991@gmail.com>
2024-08-07 17:38:18 -0300

    Fix: added src into python path directly in workflow file

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,6 @@
 import unittest
-import sys
-import os
+from src.main import main
 
-# Add the src directory to the Python path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
-
-from main import main
 
 class TestMain(unittest.TestCase):
 
