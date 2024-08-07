537a0181e2c3914caf665a8f77265dec6f828b97
Adriano <adrianocardoso1991@gmail.com>
2024-08-07 17:32:50 -0300

    Fix: fixed test file to add src directory to the python path

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,11 @@
 import unittest
-from src.main import main
+import sys
+import os
+
+# Add the src directory to the Python path
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'src')))
+
+from main import main
 
 class TestMain(unittest.TestCase):
 
