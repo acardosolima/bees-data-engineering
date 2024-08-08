b41e00d7048807fd1a817719d593e957e6bf4c85
Adriano <adrianocardoso1991@gmail.com>
2024-08-08 01:58:03 -0300

    Feat: added analysis step

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
 import logging.config
-import etl_extraction, etl_transform
+import etl_extraction, etl_transform, etl_analysis
 from modules.sources import extract_bees_brewery_api
 from modules.sinks import save_local_folder
 from modules.transformers import brewery_local
+from modules.analysis import analyze_types_location
 
 # Setup logging configuration
 logging.config.fileConfig("./logging.conf")
@@ -32,6 +33,19 @@ def main():
 
     
     print(info_silver_data)
+    silver_path = info_silver_data["full_path"]
+
+    # Analysis
+    load_func = analyze_types_location.load_silver_raw_data
+    transf_func = analyze_types_location.apply_business_logic
+    save_func = analyze_types_location.save_dataframe_as_parquet
+    partition_column = analyze_types_location.get_column_partition_by()
+
+    info_gold_data = etl_analysis.main(silver_path=silver_path, load_func=load_func,
+                                       transf_func=transf_func, save_func=save_func,
+                                       partition_column=partition_column)
+    
+    print(info_gold_data)
 
 if __name__ == "__main__":
     main()
---
+++ src/modules/analysis/analyze_types_location.py
@@ -0,0 +1,53 @@
+import logging.config
+from pyspark.sql import DataFrame, SparkSession
+from pyspark.sql.functions import col
+
+# # Setup logging configuration
+logging.config.fileConfig("logging.conf")
+logger = logging.getLogger(__name__)
+
+INPUT_PATH = "data/silver/brewery.json"
+OUTPUT_PATH = "data/gold/brewery_types_location.parquet"
+
+def load_silver_raw_data(spark: SparkSession = None, input_path: str = INPUT_PATH):
+    try:
+        df = spark.read.parquet(input_path)
+        return df
+    
+    except Exception as e:
+        print(f"Read operation failed: {e}")
+
+def apply_business_logic(df):
+
+    agg_df = df.groupBy(
+        col("BreweryType"),
+        col("StateProvince"),
+    ).count()
+
+    return agg_df
+
+def save_dataframe_as_parquet(spark: SparkSession = None, df: DataFrame = None,
+                              partition_by: str = "",
+                              output_path: str = OUTPUT_PATH) -> dict:
+    
+    response_obj = {
+        "save_successfull": False,
+        "full_path": None,
+        "size": 0
+    }
+    
+    try:
+        df.write.partitionBy(partition_by).mode("append").parquet(output_path)
+
+        logger.debug(f"Saved file {output_path} successfully")
+
+        response_obj["save_successfull"] = True
+        response_obj["full_path"] = output_path
+
+    except Exception as e:
+        print(f"Write operation failed: {e}")
+
+    return response_obj
+
+def get_column_partition_by() -> str:
+    return "StateProvince"
